479d35ca7b04fbad3600c323c2457392660b7dcc
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2023-10-06 19:09:40 -0700

    file 1 change <stdtime.h> to <time.h>

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdtime.h>
+#include <time.h>
 /**
  * main - main
  * Return: 0 (your code runs)*
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-	int n;
-	int m;
+        int n;
+        int m;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	m = n % 10;
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        m = n % 10;
 
-	if (m > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, m);
-	else if (m < 6 && m != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
-	else if (m == 0)
-		printf("Last digit of %d is %d and is zero\n", n, m);
-	return (0);
+        if (m > 5)
+                printf("Last digit of %d is %d and is greater than 5\n", n, m);
+        else if (m < 6 && m != 0)
+                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+        else if (m == 0)
+                printf("Last digit of %d is %d and is zero\n", n, m);
+        return (0);
 }
