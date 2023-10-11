a87e6037cea17405c43e6d41416716b870f78af2
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2023-10-11 15:23:04 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - this program prints _putchar followed by a newline
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+		printf(str);
+		printf('\n');
+return (0);
+}
