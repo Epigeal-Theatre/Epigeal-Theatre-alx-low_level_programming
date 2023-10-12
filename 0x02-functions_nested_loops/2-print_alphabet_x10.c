9e6c19f70b357770f94a316c299cff1b357d1f75
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2023-10-11 18:20:45 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet ten times
+ * Return: void here
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+	int i = 0;
+
+		while (i <= 9)
+		{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+			_putchar('\n');
+			i++;
+		}
+}
+
