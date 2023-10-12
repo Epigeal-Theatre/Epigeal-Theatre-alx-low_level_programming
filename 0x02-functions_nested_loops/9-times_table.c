13f9f0bc95181c771c6184b351f84d8013797154
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2023-10-11 19:56:19 -0700

    betty f9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * times_table - prints 
+ * times_table - prints
  * the 9 times table
- * Description: prints the 
+ * Description: prints the
  * 9 times table
  * Return: void
  */
