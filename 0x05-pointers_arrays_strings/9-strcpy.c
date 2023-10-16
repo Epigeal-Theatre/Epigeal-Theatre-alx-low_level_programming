6391d9a8500dc8f17ceba8cd05593d95673b4de2
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2023-10-16 13:32:54 -0700

    file 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the pointed to,str, cy the src
+ * including the terminating the 
+ * null byte (\0) to the buffer pointed to by dest
+ * @dest: the pointer to the buffer to which we copy the string
+ * @src: string to copy
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is a readme file for my 0x05 pointers, arrays and strings assignment
