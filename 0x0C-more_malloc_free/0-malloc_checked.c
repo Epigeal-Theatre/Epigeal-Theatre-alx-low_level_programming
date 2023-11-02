ae47893dbc1fe16716790e3d902238be7bee795c
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2023-11-02 16:28:33 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ *malloc_checked - use malloc to allocate mem
+ *@aaa: -
+ *@b: - size of the memory
+ *Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b);
+{
+	char *aaa;
+
+	aaa = malloc(b);
+	if (aaa == NULL)
+		exit (98);
+	else
+		return(aaa);
+}
