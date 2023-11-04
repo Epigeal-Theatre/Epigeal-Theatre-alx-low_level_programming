d1e714b1e6a84b562999ab543cfb5c79d1905ef7
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2023-11-04 01:58:39 -0700

    remove : at the end of prototype file 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,14 +9,14 @@
  *Return: NULL if compilation fails or a pointer to an array if success
  */
 
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 	char *str;
 	unsigned int i;
 
 	str = malloc(sizeof(char) * size);
 	if (size == 0 || str == NULL)
-		return(NULL);
+		return (NULL);
 	for (i = 0, i < size, i++)
 		str[i] = c;
 
