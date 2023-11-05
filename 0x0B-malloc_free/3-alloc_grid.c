91c061c23f2da3d6df8b1b4135e3e601aa67a52a
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2023-11-05 09:39:28 -0800

    file 3 and 4

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,13 +10,34 @@
 
 int **alloc_grid(int width, int height)
 {
-	int a;
+	int **zzz;
 
-	int x, y;
+	int a;
+	int b;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
+	zzz = malloc(sizeof(int *) * height);
 
+	if (zzz == NULL)
+		return (NULL);
+	for (a = 0; a < height; a++)
+	{
+		zzz[a] = malloc(sizeof(int) * width);
+		if (zzz[a] == NULL)
+		{
+			for (; a >= 0; a--)
+				free(zzz[a]);
 
+			free(zzz);
+			return (NULL);
+		}
+	}
+	for (a = 0; a < height; a++)
+	{
+		for (b = 0; b < width; b++)
+			zzz[a][b] = 0;
+	}
+	return (zzz);
 }
