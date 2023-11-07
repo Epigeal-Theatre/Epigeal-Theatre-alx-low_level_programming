800eb106e06e80ab6ae51011657d67099d0bdc6d
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2023-11-06 17:18:48 -0800

    file 2 correction

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,9 +15,9 @@ void print_dog(struct dog *d)
 
 
 	if (d->name == NULL)
-	d->name = "()";
+	d->name = "(nil)";
 	if (d->owner == NULL)
-	d->owner = "()";
+	d->owner = "(nil)";
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
